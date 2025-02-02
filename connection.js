d693c01e77c84e0d68c64da320d2492177f2e9ed
Zaheer Ahmed <ranazaheerahmed65@gmail.com>
2025-02-02 22:57:56 +0500

    updated connection.js for TiDB

--- connection.js
+++ connection.js
@@ -6,8 +6,10 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT || 4000,
+  ssl: { rejectUnauthorized: true }  // Required for TiDB Cloud
 });
+
 
 db.connect((err) => {
   if (err) {
@@ -16,5 +18,5 @@ db.connect((err) => {
     console.log('Connected to MySQL database');
   }
 });
-
+// ZHsfYjMFG5BB6clQ    password of the tidb cluster (node)
 module.exports = db;
